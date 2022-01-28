2423718bd2c03375b398dea5f2b6beb8756966b6
jefersonrgomes <jeferson.rgomes@hotmail.com>
2022-01-27 21:34:14 -0300

    create Domain, create Classes

---
+++ src/one/digitalinnovation/gof/SingletonLazy.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation.gof;
+
+/**
+ * Singleton "Preguiçoso"
+ *
+ * @author; https://github.com/jefersonrgomes
+ **/
+
+
+public class SingletonLazy {
+
+    private static SingletonLazy instancia;
+
+    private SingletonLazy(){
+
+    }
+
+}
